715989ddb24900550a14f6e55cd907029aaaa010
OlgaShopieva <shopieva94@gmail.com>
2023-08-18 03:02:42 +0300

    user-delete feature added

--- src/main/java/ru/shopieva/springcourse/controller/UserController.java
+++ src/main/java/ru/shopieva/springcourse/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.shopieva.springcourse.model.User;
 import ru.shopieva.springcourse.service.UserService;
@@ -35,6 +36,11 @@ public class UserController {
     @PostMapping("/user-create")
     public String createUser(User user){
         userService.saveUser(user);
+        return "redirect:/users";
+    }
+    @GetMapping("user-delete/{id}")   //как в браузере передаётся id для удаления пользователя?
+    public String deleteUser(@PathVariable("id") Long id){
+        userService.deleteById(id);
         return "redirect:/users";
     }
 }
